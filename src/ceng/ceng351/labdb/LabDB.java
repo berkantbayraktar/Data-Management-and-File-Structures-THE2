67f7a2dcdf157aea142e79de0c4a743e4a165b7c
bayra <berkantbayraktar06@gmail.com>
2019-12-27 22:21:56 +0300

    Correct enter and search methods according to given I/O IT WORKS WELL NOW

--- src/ceng/ceng351/labdb/LabDB.java
+++ src/ceng/ceng351/labdb/LabDB.java
@@ -23,12 +23,12 @@ public class LabDB {
 
 
     public String hash(String studentID, int depth){
-        System.out.println("depth : " + depth);
+        //System.out.println("depth : " + depth);
         String key = getNumeric(studentID);
         String binaryKey = convertToBinary(key);
-        System.out.println("BinaryKey : " + binaryKey);
+        //System.out.println("BinaryKey : " + binaryKey);
         String buckeyKey = binaryKey.substring(binaryKey.length()-depth,binaryKey.length());
-        System.out.println("Sub : " + buckeyKey);
+        //System.out.println("Sub : " + buckeyKey);
 
         return buckeyKey;
 
@@ -38,39 +38,74 @@ public class LabDB {
         String key = hash(studentID,globalDepth);
         Bucket value = buckets.get(key);
 
-        if(value == null){
-            key = hash(studentID,globalDepth-1);
-            value = buckets.get(key);
-            System.out.println("hereeeeeeeeeeeeee");
-        }
-
-
-
         if(value.getEntries().size() == this.bucketSize){
-            ArrayList<String> entries = value.getEntries();
+            if(globalDepth == value.getLocalDepth()){
+                ArrayList<String> entries = value.getEntries();
 
-            buckets.put("0" + key  , new Bucket(1 + value.getLocalDepth()));
-            buckets.put("1" + key  , new Bucket(1 + value.getLocalDepth()));
-
-            if(globalDepth == value.getLocalDepth())
+                buckets.put("0" + key  , new Bucket(1 + value.getLocalDepth()));
+                buckets.put("1" + key  , new Bucket(1 + value.getLocalDepth()));
                 globalDepth++;
+                buckets.remove(key);
 
-            buckets.remove(key);
+                for(String entry : entries){
+                    key = hash(entry,globalDepth);
+                    buckets.get(key).insertEntry(entry);
+                }
 
-            for(String entry : entries){
-                key = hash(entry,globalDepth);
-                buckets.get(key).insertEntry(entry);
+                key = hash(studentID,globalDepth);
+                buckets.get(key).insertEntry(studentID);
+
+                Set<String> kS = this.buckets.keySet();
+
+                ArrayList<String> keySet = new ArrayList<>();
+                for(String currentkey : kS){
+                    keySet.add(currentkey);
+                }
+
+                for(String currentkey : keySet){
+                    if(this.buckets.get(currentkey).getLocalDepth()< globalDepth){
+                        Bucket bucket = this.buckets.remove(currentkey);
+                        this.buckets.put("1" + currentkey, bucket);
+                        this.buckets.put("0" + currentkey, bucket);
+                    }
+                }
             }
 
-            key = hash(studentID,globalDepth);
-            buckets.get(key).insertEntry(studentID);
+            else{
+                ArrayList<String> entries = new ArrayList<>();
 
+                for(int i = 0 ; i < value.getEntries().size() ; i++){
+                    entries.add(value.getEntries().get(i));
+                }
+
+
+                if(key.startsWith("1")){
+                    buckets.remove(key);
+                    buckets.remove("0" + key.substring(1));
+
+                    buckets.put( key  , new Bucket(globalDepth));
+                    buckets.put("0" + key.substring(1), new Bucket(globalDepth));
+                }
+                else if(key.startsWith("0")){
+                    buckets.remove(key);
+                    buckets.remove("1" + key.substring(1));
+
+                    buckets.put( key  , new Bucket(globalDepth));
+                    buckets.put("1" + key.substring(1), new Bucket(globalDepth));
+                }
+
+                for(String entry : entries){
+                    key = hash(entry,globalDepth);
+                    buckets.get(key).insertEntry(entry);
+                }
+
+                key = hash(studentID,globalDepth);
+                buckets.get(key).insertEntry(studentID);
+            }
         }
         else{
             buckets.get(key).insertEntry(studentID);
         }
-
-
     }
 
     public void leave(String studentID) {
@@ -78,12 +113,25 @@ public class LabDB {
     }
 
     public String search(String studentID) {
-        return "";
+        String key = hash(studentID,globalDepth);
+        Set<String> keys = buckets.keySet();
+        System.out.println(keys);
+        Bucket bucket = this.buckets.get(key);
+        ArrayList<String> entries = bucket.getEntries();
+
+        for(String entry : entries){
+            if (studentID.equals(entry)) {
+                return key;
+            }
+        }
+
+        return "-1";
     }
 
     public void printLab() {
         System.out.println("Global depth : " + this.globalDepth);
         TreeMap<String,Bucket> map = new TreeMap<>();
+        /*
         Set<String> kS = this.buckets.keySet();
 
         ArrayList<String> keySet = new ArrayList<>();
@@ -97,7 +145,7 @@ public class LabDB {
                 this.buckets.put("1" + key, bucket);
                 this.buckets.put("0" + key, bucket);
             }
-        }
+        }*/
 
         map.putAll(this.buckets);
         map.forEach((key,value) -> System.out.println(key + " : " + "[Local depth:" + value.getLocalDepth() + "]" + value.printEntries()));
